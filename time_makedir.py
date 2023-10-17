21673ea42b9e11f99de3af25faa1581aac071652
squareforce4 <148249151+squareforce4@users.noreply.github.com>
2023-10-17 23:43:01 +0900

    Add files via upload

---
+++ time_makedir.py
@@ -0,0 +1,24 @@
+import datetime
+import time
+import subprocess
+import pytz
+
+# JSTタイムゾーンを設定する
+JST = pytz.timezone('Asia/Tokyo')
+
+# 実行する時間を設定する（例：毎日の午後3時）
+target_time = datetime.time(hour=0, minute=0, second=0)
+print("target", target_time)
+
+while True:
+    # 現在の時刻を取得する
+    now = datetime.datetime.now(JST).time()
+    print("now", now)
+
+    # 実行する時間になったらスクリプトを実行する
+    if now >= target_time:
+        subprocess.run(['python', 'makedir.py'])
+        # 待つ
+        time.sleep(24 * 60 * 60)
+    else:
+        time.sleep(60)
