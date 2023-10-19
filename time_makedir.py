41057b34e2b5cf576189b276a8232001c53bd369
squareforce4 <148249151+squareforce4@users.noreply.github.com>
2023-10-19 21:00:50 +0900

    Update time_makedir.py

--- time_makedir.py
+++ time_makedir.py
@@ -6,7 +6,7 @@ import pytz
 # JSTタイムゾーンを設定する
 JST = pytz.timezone('Asia/Tokyo')
 
-# 実行する時間を設定する（例：毎日の午後5時）
+# 実行する時間を設定する（例：毎日の午後05時）
 target_time = datetime.time(hour=0, minute=0, second=0)
 print("target", target_time)
 
